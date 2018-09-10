740e6c4802cf87242257759741b24def8476a820
robschwitzer <deadinthewater@me.com>
2018-09-10 15:20:13 -0700

    set up redux store and apollo client

--- client/App.js
+++ client/App.js
@@ -8,6 +8,47 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createHttpLink } from 'apollo-link-http';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
+import ReduxLink from 'apollo-link-redux';
+import { onError } from 'apollo-link-error';
+
+const URL = 'localhost:8080'
+
+const store = createStore(
+  combineReducers({
+    apollo: apolloReducer,
+  }),
+  {}, //initial state
+  composeWithDevTools(),
+)
+
+const cache = new ReduxCache({ store })
+
+const reduxLink = new ReduxLink(store)
+
+const errorLink = onError((errors) => {
+  console.log(`💣💣💣 ${errors}`)
+})
+
+const httpLink = createHttpLink({ uri: `http://${URL}` })
+
+const link = ApolloLink.from([
+  reduxLink,
+  errorLink,
+  httpLink,
+])
+
+export const client = new ApolloClient({
+  link,
+  cache,
+})
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,11 +61,15 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome to React Native!</Text>
+            <Text style={styles.instructions}>To get started, edit App.js</Text>
+            <Text style={styles.instructions}>{instructions}</Text>
+          </View>
+        </Provider>
+      </ApolloProvider>
     );
   }
 }
