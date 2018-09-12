5a2570f675224533b8989cad7f68586a7abace5d
robschwitzer <deadinthewater@me.com>
2018-09-12 15:23:58 -0700

    connected to sqlite db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 // import { ApolloServer, gql } from 'apollo-server'
 import { ApolloServer } from 'apollo-server'
 import { typeDefs } from './data/schema'
-import { mocks } from './data/mocks'
+// import { mocks } from './data/mocks'
+import { resolvers } from './data/resolvers'
 const PORT = 8080
 
-const server = new ApolloServer({ typeDefs, mocks })
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  // mocks,
+})
 
 server.listen({ port: PORT }).then(({ url }) => console.log(`🚀🚀🚀 Server ready at ${url}`))
 
